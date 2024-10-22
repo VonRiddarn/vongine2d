f5be689df71896a4f4d51ba43c5ffd6656bdc919
VonRiddarn <vonriddarn@gmail.com>
2024-10-22 18:53:39 +0200

    feat: prepared structure with a tick and render manager

---
+++ engine/RenderManager.cs
@@ -0,0 +1,7 @@
+namespace vongine2d
+{
+	public sealed class RenderManager
+	{
+		// Centralized class for rendering spriterenderers based on their layer and order.
+	}
+}
---
+++ engine/TickManager.cs
@@ -0,0 +1,7 @@
+namespace vongine2d
+{
+	public sealed class TickManager
+	{
+		// Centralized class for invoking the Update methods
+	}
+}
